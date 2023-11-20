06c0fe8b0b40faa12212ad96a366df2b475824ce
Boti <budai.boti03@gmail.com>
2023-11-20 15:07:26 +0100

    idk what i did

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import App from './App'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
 
-ReactDOM.render(
-  <Provider store={store}>
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
-)
+);
+reportWebVitals();
