799a0fbbf0f410a1e4ecead0aa1116926947965c
Boti <budai.boti03@gmail.com>
2023-11-20 16:22:26 +0100

    xdwerrwe

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment } from './counterSlice'
-
+//test
 export function Counter() {
   const count = useSelector((state) => state.counter.value)
   const dispatch = useDispatch()
