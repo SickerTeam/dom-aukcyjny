428b223c87492655ac5932c767bd43f44a445d00
Boti <budai.boti03@gmail.com>
2023-11-22 14:17:36 +0100

    Listings

--- frontend/components/index.ts
+++ frontend/components/index.ts
@@ -1,10 +1,14 @@
 import Main from './Main';
 import CustomButton from './CustomButton'
 import Navbar from './Navbar';
+import Artworks from './Artworks';
+import Filters from './Filters';
 
 export {
     Main,
     CustomButton,
     Navbar,
+    Artworks,
+    Filters,
 }
  
