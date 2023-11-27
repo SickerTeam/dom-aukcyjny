904d7d61d0c451827b05d61204d9ddff12e3c487
DoubiOD <10407554@ucn.dk>
2023-11-27 14:19:49 +0100

    User DTOs
    "User.cs", "UserDTO" (no password), "UserRegistrationDTO" (no id)

--- backend/DTOs/UserDTO.cs
+++ backend/DTOs/UserDTO.cs
@@ -1,6 +1,29 @@
-﻿namespace backend.DTOs
+﻿using backend.Models;
+
+namespace backend.DTOs
 {
     public class UserDTO
     {
+        // User but no password
+        public UserDTO(int id, string email, string firstName, string lastName, string bio, string country, string link, UserRole role)
+        {
+            this.Id = id;
+            this.Email = email;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Bio = bio;
+            this.Country = country;
+            this.Link = link;
+            this.Role = role;
+        }
+
+        private int Id { get; set; }
+        private string Email { get; set; }
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private string Bio { get; set; }
+        private string Country { get; set; }
+        private string Link { get; set; }
+        private UserRole Role { get; set; }
     }
 }
--- backend/DTOs/UserRegisterationDTO.cs
+++ backend/DTOs/UserRegisterationDTO.cs
@@ -1,11 +1,12 @@
-﻿namespace backend.Models
+﻿using backend.Models;
+
+namespace backend.DTOs
 {
-    public class User
+    public class UserRegisterationDTO
     {
-        // All firlds including Id & Password
-        public User(int id, string email, string password, string firstName, string lastName, string bio, string country, string link, UserRole role)
+        // user but no ID
+        public UserRegisterationDTO(string email, string password, string firstName, string lastName, string bio, string country, string link, UserRole role)
         {
-            this.Id = id;
             this.Email = email;
             this.Password = password;
             this.FirstName = firstName;
@@ -15,13 +16,11 @@
             this.Link = link;
             this.Role = role;
         }
-
-        private int Id { get; set; }
-        private string Email {  get; set; }
+        private string Email { get; set; }
         private string Password { get; set; }
         private string FirstName { get; set; }
         private string LastName { get; set; }
-        private string Bio {  get; set; }
+        private string Bio { get; set; }
         private string Country { get; set; }
         private string Link { get; set; }
         private UserRole Role { get; set; }
