008164610509defc1b70b0d79c5efc406bf71383
Gabriel Pędlowski <93519015+g-ped@users.noreply.github.com>
2023-11-28 15:02:05 +0100

    some random changes

--- frontend/components/Artworks.tsx
+++ frontend/components/Artworks.tsx
@@ -1,20 +1,30 @@
-import ArtworkCard from './ArtworkCard'
+"use client";
+
+import { useRouter } from "next/navigation";
+import ArtworkCard from "./ArtworkCard";
 
 interface IArtworks {
-  artworks: any[]
+  artworks: any[];
 }
 
-const Artworks = ({artworks}: IArtworks) => {
-    
+const Artworks = ({ artworks }: IArtworks) => {
+  const router = useRouter();
+
+  const handleOnCardClick = (id: number) => {
+    router.push(`/artworks/${id}`);
+  };
+
   return (
     <div className="grid grid-cols-5 gap-4 m-10">
       {artworks.map((artwork, index) => (
-        <div key={index}>
-          <ArtworkCard artwork={artwork}/>
-        </div>
+        <ArtworkCard
+          key={index}
+          artwork={artwork}
+          handleClick={() => handleOnCardClick(artwork.id)}
+        />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Artworks
+export default Artworks;
--- frontend/src/app/artworks/page.tsx
+++ frontend/src/app/artworks/page.tsx
@@ -1,19 +1,21 @@
-import Image from 'next/image'
-import { Artworks, Filters } from '../../../components'
+import Image from "next/image";
+import { Artworks, Filters } from "../../../components";
+import Path from "../../../components/Path";
 
 export default function ArtworksList() {
-
-  const artworks =  Array.from({ length: 100 }, (_, index) => ({
+  const artworks = Array.from({ length: 100 }, (_, index) => ({
+    id: index,
     title: `Mona Lisa by Leonardo da Vinci (1503 - 1506)`,
     price: 1300000, // You can modify this if you want random prices
     time: 10, // You can modify this if you want random times
   }));
-  
+
   return (
-    <main className='overflow-hidden'>
+    <main className="overflow-hidden">
+      <Path />
       <Filters />
 
-      <Artworks artworks={artworks}/>
+      <Artworks artworks={artworks} />
     </main>
-  )
+  );
 }
