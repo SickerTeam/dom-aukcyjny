768359321f23e18d9b099929127e1acc71bd168b
Gabriel Pędlowski <93519015+g-ped@users.noreply.github.com>
2023-11-30 19:44:57 +0100

    maybe fix

--- frontend/components/Auctions/AuctionsListPage.tsx
+++ frontend/components/Auctions/AuctionsListPage.tsx
@@ -1,25 +1,12 @@
 import { Artworks, Filters, Path } from "..";
 
-async function getAllAuctions() {
-  const res = await fetch("http://localhost:5156/auctions");
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch auctions");
-  }
-
-  return res.json();
-}
-
 const AuctionsListPage = async () => {
-  // const artworks = Array.from({ length: 100 }, (_, index) => ({
-  //   id: index,
-  //   title: `Mona Lisa by Leonardo da Vinci (1503 - 1506)`,
-  //   price: 1300000, // You can modify this if you want random prices
-  //   time: 10, // You can modify this if you want random times
-  // }));
-
-  const auctions = await getAllAuctions();
-  console.log(auctions);
+  const auctions = Array.from({ length: 100 }, (_, index) => ({
+    id: index,
+    title: `Mona Lisa by Leonardo da Vinci (1503 - 1506)`,
+    price: 1300000, // You can modify this if you want random prices
+    time: 10, // You can modify this if you want random times
+  }));
 
   return (
     <div>
