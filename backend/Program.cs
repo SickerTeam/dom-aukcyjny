8e1c9ba30f7ade69cc96a7f065e95551fc5e1ddc
MastBlast <martin9music@gmail.com>
2023-12-07 21:29:34 +0100

    Removed config from gh

--- backend/Program.cs
+++ backend/Program.cs
@@ -2,6 +2,8 @@ using AutoMapper;
 using backend.Data;
 using backend.Repositories;
 using backend.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace backend
 {
@@ -10,12 +12,15 @@ namespace backend
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var configuration = builder.Configuration;
 
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            builder.Services.AddDbContext<DatabaseContext>();
+            builder.Services.AddDbContext<DatabaseContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DatabaseContext")));
+                
             builder.Services.AddScoped<IUserRepository, UserRepository>();
             builder.Services.AddScoped<IInstaBuyRepository, InstaBuyRepository>();
             builder.Services.AddScoped<IUserService, UserService>();
