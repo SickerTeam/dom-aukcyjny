898ab4f685bc9f307ff1ff3893a04667421005d9
Gabriel Pędlowski <93519015+g-ped@users.noreply.github.com>
2023-12-12 13:06:24 +0100

    ye

--- frontend/components/Artworks.tsx
+++ frontend/components/Artworks.tsx
@@ -5,17 +5,14 @@ import ArtworkCard from "./ArtworkCard";
 import apiService from "../services/apiService";
 
 const Artworks = async () => {
-  // const [auctions, setAuctions] = useState<[]>([]);
+  const [auctions, setAuctions] = useState<[]>([]);
 
-  const auctions = await apiService.get(`/auction`);
-  console.log(auctions);
-
-  // useEffect(() => {
-  //   apiService
-  //     .get(`/auction`) // still need to change the names of controllers xd
-  //     .then((data) => setAuctions(data))
-  //     .catch((error) => console.error("Error: ", error));
-  // }, []);
+  useEffect(() => {
+    apiService
+      .get(`/auctions`)
+      .then((data) => setAuctions(data))
+      .catch((error) => console.error("Error: ", error));
+  }, []);
 
   return (
     <div className="grid grid-cols-6 gap-4">
--- frontend/components/Auctions/AuctionsListPage.tsx
+++ frontend/components/Auctions/AuctionsListPage.tsx
@@ -1,19 +1,6 @@
 import { Artworks, Filters, Path } from "..";
-import apiService from "../../services/apiService";
-
-async function getAllAuctions() {
-  const res = await apiService.get("/auctions");
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch auctions");
-  }
-
-  return res.json();
-}
 
 const AuctionsListPage = async () => {
-  const auctions = await getAllAuctions();
-
   return (
     <div className="px-40">
       <Path />
