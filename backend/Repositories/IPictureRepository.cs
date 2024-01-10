dd84e492fe8c5892cbe1e19bb4f96f631deecbdd
MastBlast <martin9music@gmail.com>
2024-01-10 20:21:20 +0100

    uncommented classes

--- backend/Repositories/IPictureRepository.cs
+++ backend/Repositories/IPictureRepository.cs
@@ -1,12 +1,12 @@
-﻿// using backend.Data.Models;
+﻿using backend.Data.Models;
 
-// namespace backend.Repositories
-// {
-//     public interface IPictureRepository
-//     {
-//         Task<IEnumerable<DbPicture>> GetPictureAsync();
-//         Task<DbPicture> GetPicturesByIdAsync(int id);
-//         Task AddPictureAsync(DbPicture picture);
-//         Task DeletePictureAsync(DbPicture picture);
-//     }
-// }
+namespace backend.Repositories
+{
+    public interface IPictureRepository
+    {
+        Task<IEnumerable<DbPicture>> GetPictureAsync();
+        Task<DbPicture> GetPicturesByIdAsync(int id);
+        Task AddPictureAsync(DbPicture picture);
+        Task DeletePictureAsync(DbPicture picture);
+    }
+}
