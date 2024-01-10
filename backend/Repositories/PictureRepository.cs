bc113c82e64a4ac055adb76850e24de772fc19e4
DoubiOD <10407554@ucn.dk>
2024-01-10 20:16:07 +0100

    Not using services and repos

--- backend/Repositories/PictureRepository.cs
+++ backend/Repositories/PictureRepository.cs
@@ -1,33 +1,33 @@
-﻿using backend.Data;
-using backend.Data.Models;
+﻿// using backend.Data;
+// using backend.Data.Models;
 
-namespace backend.Repositories
-{
-    public class PictureRepository(DatabaseContext context) : IPictureRepository
-    {
-        private readonly DatabaseContext _context = context;
+// namespace backend.Repositories
+// {
+//     public class PictureRepository(DatabaseContext context) : IPictureRepository
+//     {
+//         private readonly DatabaseContext _context = context;
         
-        public async Task<IEnumerable<DbPicture>> GetPictureAsync()
-        {
-            return await _context.Pictures.ToListAsync();
-        }
+//         public async Task<IEnumerable<DbPicture>> GetPictureAsync()
+//         {
+//             return await _context.Pictures.ToListAsync();
+//         }
 
-        public async Task<DbPicture> GetPicturesByIdAsync(int id)
-        {
-            DbPicture? picture = await _context.Pictures.FindAsync(id);
-            return picture ?? throw new ArgumentException("Picture not found");            
-        }
+//         public async Task<DbPicture> GetPicturesByIdAsync(int id)
+//         {
+//             DbPicture? picture = await _context.Pictures.FindAsync(id);
+//             return picture ?? throw new ArgumentException("Picture not found");            
+//         }
 
-        public async Task AddPictureAsync(DbPicture picture)
-        {
-            _context.Pictures.Add(picture);
-            await _context.SaveChangesAsync();
-        }
+//         public async Task AddPictureAsync(DbPicture picture)
+//         {
+//             _context.Pictures.Add(picture);
+//             await _context.SaveChangesAsync();
+//         }
 
-        public async Task DeletePictureAsync(DbPicture picture)
-        {         
-            _context.Pictures.Remove(picture);
-            await _context.SaveChangesAsync();
-        }
-    }
-}
+//         public async Task DeletePictureAsync(DbPicture picture)
+//         {         
+//             _context.Pictures.Remove(picture);
+//             await _context.SaveChangesAsync();
+//         }
+//     }
+// }
--- backend/Services/PictureService.cs
+++ backend/Services/PictureService.cs
@@ -1,40 +1,40 @@
-﻿using AutoMapper;
-using backend.DTOs;
-using backend.Data.Models;
-using backend.Repositories;
+﻿// using AutoMapper;
+// using backend.DTOs;
+// using backend.Data.Models;
+// using backend.Repositories;
 
-namespace backend.Services
-{
-   public class PictureService(IPictureRepository pictureRepository, IMapper mapper) : IPictureService
-   {
-        private readonly IPictureRepository _pictureRepository = pictureRepository;
-        private readonly IMapper _mapper = mapper;
+// namespace backend.Services
+// {
+//    public class PictureService(IPictureRepository pictureRepository, IMapper mapper) : IPictureService
+//    {
+//         private readonly IPictureRepository _pictureRepository = pictureRepository;
+//         private readonly IMapper _mapper = mapper;
 
-        public async Task<IList<PictureDTO>> GetPicturesAsync()
-        {
-            IEnumerable<DbPicture> pictures = await _pictureRepository.GetPictureAsync();
-            return _mapper.Map<IList<PictureDTO>>(pictures);
-        }
+//         public async Task<IList<PictureDTO>> GetPicturesAsync()
+//         {
+//             IEnumerable<DbPicture> pictures = await _pictureRepository.GetPictureAsync();
+//             return _mapper.Map<IList<PictureDTO>>(pictures);
+//         }
 
-        public async Task<PictureDTO> GetPictureByIdAsync(int id)
-        {
-            DbPicture picture = await _pictureRepository.GetPicturesByIdAsync(id);
-            return _mapper.Map<PictureDTO>(picture);
-        }
+//         public async Task<PictureDTO> GetPictureByIdAsync(int id)
+//         {
+//             DbPicture picture = await _pictureRepository.GetPicturesByIdAsync(id);
+//             return _mapper.Map<PictureDTO>(picture);
+//         }
 
-        public async Task AddPictureAsync(PictureCreationDTO commentDto)
-        {
-            DbPicture picture = _mapper.Map<DbPicture>(commentDto);
-            picture.CreatedAt = DateTime.UtcNow;
-            await _pictureRepository.AddPictureAsync(picture);
-        }
+//         public async Task AddPictureAsync(PictureCreationDTO commentDto)
+//         {
+//             DbPicture picture = _mapper.Map<DbPicture>(commentDto);
+//             picture.CreatedAt = DateTime.UtcNow;
+//             await _pictureRepository.AddPictureAsync(picture);
+//         }
 
-        public async Task DeletePictureAsync(int id)
-        {
-            DbPicture picture = await _pictureRepository.GetPicturesByIdAsync(id);
-            if (picture == null) return;
+//         public async Task DeletePictureAsync(int id)
+//         {
+//             DbPicture picture = await _pictureRepository.GetPicturesByIdAsync(id);
+//             if (picture == null) return;
 
-            await _pictureRepository.DeletePictureAsync(picture);
-        }
-    }
-}
+//             await _pictureRepository.DeletePictureAsync(picture);
+//         }
+//     }
+// }
