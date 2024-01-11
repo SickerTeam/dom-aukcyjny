a2c032fe495192751a782e2929e894ce250e1dbf
MastBlast <martin9music@gmail.com>
2024-01-11 19:00:37 +0100

    add anal

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { inject } from "@vercel/analytics";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 reportWebVitals();
-inject();
